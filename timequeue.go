9396e108f7c7399f8bd295633439425210f959bb
Eric Elsken <eric@pmg.co>
2015-10-22 15:51:37 -0500

    Adds all untested functionality.

--- timequeue.go
+++ timequeue.go
@@ -8,16 +8,19 @@ import (
 )
 
 const (
-	DefaultSize = 128
+	DefaultSize = 1
 )
 
 type TimeQueue struct {
 	*sync.RWMutex
 	messageHeap
-	running bool
-	stop    chan struct{}
+	running  bool
+	stop     chan struct{}
+	nextTime time.Time
+	nextDone chan struct{}
 
 	messageChan chan *Message
+	wakeChan    chan struct{}
 }
 
 func New() *TimeQueue {
@@ -31,23 +34,33 @@ func NewSize(size int) *TimeQueue {
 		running:     false,
 		stop:        nil,
 		messageChan: make(chan *Message, size),
+		wakeChan:    make(chan struct{}),
 	}
 	heap.Init(&q.messageHeap)
 	return q
 }
 
 func (q *TimeQueue) Push(time time.Time, data []byte) {
+	q.PushMessage(&Message{
+		Time: time,
+		Data: data,
+	})
 }
 
-func (q *TimeQueue) PushMessage(time time.Time, message *Message) {
+func (q *TimeQueue) PushMessage(message *Message) {
+	q.enqueue(message)
 }
 
 func (q *TimeQueue) Peek() (time.Time, []byte) {
-	return time.Now(), nil
+	message := q.PeekMessage()
+	return message.Time, message.Data
 }
 
 func (q *TimeQueue) PeekMessage() *Message {
-	return nil
+	q.RLock()
+	defer q.RUnlock()
+	message := q.messageHeap[0]
+	return message
 }
 
 func (q *TimeQueue) Messages() <-chan *Message {
@@ -75,12 +88,63 @@ func (q *TimeQueue) Stop() {
 	go func() {
 		q.stop <- struct{}{}
 	}()
+	<-q.stop
+}
+
+func (q *TimeQueue) wake() {
+	go func() {
+		q.wakeChan <- struct{}{}
+	}()
 }
 
 func (q *TimeQueue) run() {
 	for {
-		sleep := time.Duration(1) * time.Second
-		fmt.Println("awake")
-		time.Sleep(sleep)
+		select {
+		case <-q.wakeChan:
+			q.dequeue()
+		case <-q.stop:
+			q.stop <- struct{}{}
+			break
+		}
+	}
+}
+
+func (q *TimeQueue) enqueue(message *Message) {
+	q.Lock()
+	defer q.Unlock()
+	if len(q.messageHeap) > 0 {
+		nextTime := q.messageHeap[0].Time
+		if message.Time.Sub(nextTime) < 0 {
+			q.nextDone <- struct{}{}
+			<-q.nextDone
+		}
+	}
+	heap.Push(&q.messageHeap, message)
+	nextTime := q.messageHeap[0]
+	nextTimeChan := time.After(nextTime.Sub(time.Now()))
+	go sendSignal(q.wakeChan, nextTimeChan, q.nextDone)
+}
+
+func (q *TimeQueue) dequeue() {
+	q.Lock()
+	defer q.Unlock()
+	if len(q.messageHeap) == 0 {
+		return
+	}
+	message := heap.Pop(&q.messageHeap).(*Message)
+	fmt.Println(message)
+	go q.deploy(message)
+}
+
+func (q *TimeQueue) deploy(message *Message) {
+	q.messageChan <- message
+}
+
+func sendSignal(dst chan struct{}, src <-chan time.Time, done chan struct{}) {
+	select {
+	case <-src:
+		dst <- struct{}{}
+	case <-done:
+		done <- struct{}{}
 	}
 }
