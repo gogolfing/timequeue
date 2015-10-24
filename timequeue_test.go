e703b79e42df4d0f5a2437746f824226ae98452e
Eric Elsken <eric@pmg.co>
2015-10-24 16:44:13 -0500

    Adds tests for Push*() methods.

--- timequeue_test.go
+++ timequeue_test.go
@@ -1,11 +1,14 @@
 package timequeue
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestNew(t *testing.T) {
 	q := New()
 	if cap(q.messageChan) != DefaultSize {
-		t.Fail()
+		t.Errorf("cap(messageChan) = %v WANT %v", cap(q.messageChan), DefaultSize)
 	}
 }
 
@@ -31,5 +34,49 @@ func TestNewSize(t *testing.T) {
 	}
 	if q.wakeChan == nil || q.stopChan == nil {
 		t.Errorf("NewSize() wakeChan and stopChan should be non-nil")
+	}
+}
+
+func TestPush(t *testing.T) {
+	q := New()
+	q.Push(time.Time{}, "test_data")
+	size := q.messageHeap.Len()
+	if size != 1 {
+		t.Errorf("q.messageHeap.Len() = %v WANT %v", size, 1)
+	}
+	message := q.messageHeap[0]
+	if message == nil {
+		t.Errorf("message = nil WANT non-nil")
+	}
+	if !message.Time.Equal(time.Time{}) {
+		t.Errorf("message.Time = %v WANT %v", message.Time, time.Time{})
+	}
+	if message.Data != "test_data" {
+		t.Errorf("message.Data = %v WANT %v", message.Data, "test_data")
+	}
+}
+
+func TestPushMessage_nil(t *testing.T) {
+	q := New()
+	q.PushMessage(nil)
+	size := q.messageHeap.Len()
+	if size != 0 {
+		t.Errorf("q.messageHeap.Len() = %v WANT %v", size, 0)
+	}
+}
+
+func TestPushMessage_nonNil(t *testing.T) {
+	q := New()
+	message := &Message{
+		Time: time.Time{},
+		Data: "test_data",
+	}
+	q.PushMessage(message)
+	size := q.messageHeap.Len()
+	if size != 1 {
+		t.Errorf("q.messageHeap.Len() = %v WANT %v", size, 1)
+	}
+	if q.messageHeap[0] != message {
+		t.Errorf("q.messageHeap[0] = %v WANT %v", q.messageHeap[0], message)
 	}
 }
