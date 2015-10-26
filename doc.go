be4cfb24f00f89db24af2fcd790914c5e60fe4ab
Eric Elsken <eric@pmg.co>
2015-10-26 10:40:40 -0500

    Updates package documentation.

--- doc.go
+++ doc.go
@@ -5,10 +5,14 @@
 //Messages need only be pushed to the queue, and then when their time.Time passes (via time.After()),
 //they will be sent on the channel returned by Messages(). See below for examples.
 //
+//Messages that are "released" (sent on the Messages() channel) are always released from a newly spawned go-routine
+//so that the main, running routine does not hang and all messages are sent asynchronously to the Messages() channel.
+//Messages with the same time.Time value will be "flood-released" at the same time from the separately spawned go-routine.
+//
 //The TimeQueue uses a single go-routine to receive the time the earliest Message is supposed to be released (via time.After()).
 //Whenever a Message has been released, the next earliest Message is then used to determine the next time to wake
 //and release the Message.
-//Messages that are released are always released from a newly spawned go-routine so that the main, running routine does
-//not hang and all messages are sent asynchronously to Messages() channel.
-//Messages with the same time.Time value will be "flood-released" at the same time from the same, separate go-routine.
+//Messages that are pushed with earlier times than the earliest already in the queue will be pushed to the front of the queue
+//and released appropriately.
+//Messages that are pushed with times before time.Now() will immediately be released from the the queue.
 package timequeue
