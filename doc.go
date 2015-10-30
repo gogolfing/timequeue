cdf36f7229867b5e35b1843a4e96111d253e03fe
Eric Elsken <eric@pmg.co>
2015-10-30 14:36:35 -0500

    Updates package documentation.

--- doc.go
+++ doc.go
@@ -1,19 +1,33 @@
 //Package timequeue provides the TimeQueue type that is a queue of Messages.
-//Each Message contains a time.Time that describes the time at which the Message should be released from the queue.
-//Message types also have a Data field of type interface{} that should be used as the payload of the Message.
+//Each Message contains a time.Time that describes the time at which the Message
+//should be released from the queue.
+//Message types also have a Data field of type interface{} that should be used
+//as the payload of the Message.
 //TimeQueue is safe for use by multiple go-routines.
 //
-//Messages need only be pushed to the queue, and then when their time.Time passes (via time.After()),
-//they will be sent on the channel returned by Messages(). See below for examples.
+//Messages need only be pushed to the queue, and then when their time passes,
+//they will be sent on the channel returned by Messages().
+//See below for examples.
+
+//TimeQueue uses a single go-routine, spawned from Start() that returns from Stop(),
+//that processes the Messages as their times pass.
+//When a Message is pushed to the queue, the earliest Message in the queue is
+//used to determine the next time the running go-routine should wake.
+//The running go-routine knows when to wake because the earliest time is used
+//to make a channel via time.After(). Receiving on that channel wakes the
+//running go-routine, is a call to Stop() does not happen prior.
+//Upon waking, that Message is removed from the queue and released on the channel
+//returned from Messages().
+//Then the newest remaining Message is used to determine when to wake, etc.
+//If a Message with a time before any other in the queue are inserted, then that
+//Message is pushed to the front of the queue and released appropriately.
 //
-//Messages that are "released" (sent on the Messages() channel) are always released from a newly spawned go-routine
-//so that the main, running routine does not hang and all messages are sent asynchronously to the Messages() channel.
-//Messages with the same time.Time value will be "flood-released" at the same time from the separately spawned go-routine.
+//Message that are "released", i.e. send on the Messages() channel, are always
+//released from a newly spawned go-routine so that other go-routines are not
+//paused waiting for a receive from Messages().
 //
-//The TimeQueue uses a single go-routine to receive the time the earliest Message is supposed to be released (via time.After()).
-//Whenever a Message has been released, the next earliest Message is then used to determine the next time to wake
-//and release the Message.
-//Messages that are pushed with earlier times than the earliest already in the queue will be pushed to the front of the queue
-//and released appropriately.
-//Messages that are pushed with times before time.Now() will immediately be released from the the queue.
+//Messages with the same Time value will be "flood-released" from the same
+//separately spawned go-routine.
+//Additionally, Messages that are pushed with times before time.Now() will
+//immediately be released from the queue.
 package timequeue
